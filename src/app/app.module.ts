c2191f76105ecaf9a6e11dfab4bfb35e46d729aa
Alex Perry <alex.perry422@gmail.com>
2023-09-02 22:40:52 -0400

    removed old unused components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PromptControllerComponent } from './prompt-controller/prompt-controller.component';
-import { PainPromptComponent } from './pain-prompt/pain-prompt.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -25,8 +23,6 @@ import { ResultsComponent } from './results/results.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PromptControllerComponent,
-    PainPromptComponent,
     MetadataFormComponent,
     QuestionnaireComponent,
     ResultsComponent,
